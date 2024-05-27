f476583525633b12cbdea08bd3ba131c0043ee7b
barami62 <wodbs7758@naver.com>
2024-05-27 14:24:02 +0900

    Feat: JPA Auditing 설정 추가

---
+++ src/main/java/ex/boards/global/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package ex.boards.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
