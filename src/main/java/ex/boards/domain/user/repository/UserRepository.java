aecd8da30ddfd9e32de8ef8721881defbe9361aa
barami62 <wodbs7758@naver.com>
2024-05-27 15:07:00 +0900

    Feat: 유저 리포지토리 추가

---
+++ src/main/java/ex/boards/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ex.boards.domain.user.repository;
+
+import ex.boards.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
