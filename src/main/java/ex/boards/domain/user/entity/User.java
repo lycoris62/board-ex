b882c8ecce5c71c988f667013c69f7f4b6ea5ed3
barami62 <wodbs7758@naver.com>
2024-05-27 14:25:09 +0900

    Feat: 유저 엔티티 추가

---
+++ src/main/java/ex/boards/domain/user/entity/User.java
@@ -0,0 +1,40 @@
+package ex.boards.domain.user.entity;
+
+import ex.boards.domain.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 최소 접근 제어자 사용
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nickname", nullable = false, length = 20)
+    private String nickname;
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 60) // BCrypt 길이는 60
+    private String password;
+
+    @Builder
+    private User(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+}
