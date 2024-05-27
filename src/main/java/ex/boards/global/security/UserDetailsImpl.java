aa4637696fc9fea4766491875a17b049bfbef4c0
barami62 <wodbs7758@naver.com>
2024-05-27 17:55:34 +0900

    Feat: UserDetails 구현

---
+++ src/main/java/ex/boards/global/security/UserDetailsImpl.java
@@ -0,0 +1,31 @@
+package ex.boards.global.security;
+
+import ex.boards.domain.user.entity.User;
+import java.util.Collection;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> user.getRole().getAuthority());
+    }
+}
