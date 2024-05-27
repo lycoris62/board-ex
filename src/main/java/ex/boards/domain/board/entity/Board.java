e499ba6ee1a492238b21b2ccbd60af24db5e4ca7
barami62 <wodbs7758@naver.com>
2024-05-27 15:40:12 +0900

    Feat: 게시판 엔티티 추가

---
+++ src/main/java/ex/boards/domain/board/entity/Board.java
@@ -0,0 +1,45 @@
+package ex.boards.domain.board.entity;
+
+import ex.boards.domain.model.BaseEntity;
+import ex.boards.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "boards")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "is_public", nullable = false)
+    private boolean isPublic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User creator;
+
+    @Builder
+    private Board(String name, boolean isPublic, User creator) {
+        this.name = name;
+        this.isPublic = isPublic;
+        this.creator = creator;
+    }
+}
