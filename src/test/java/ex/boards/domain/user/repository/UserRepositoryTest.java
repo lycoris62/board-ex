276da25d45312b59f5e6a18ae27c342afab738d5
barami62 <wodbs7758@naver.com>
2024-05-27 15:37:25 +0900

    Test: 유저 리포지토리 테스트에 이메일 존재여부 확인 케이스 추가

--- src/test/java/ex/boards/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/ex/boards/domain/user/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package ex.boards.domain.user.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import ex.boards.domain.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,8 @@ class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    @DisplayName("이메일로 유저 검색 성공")
-    @Test
-    void find_by_email_success() {
-        // given
+    @BeforeEach
+    void setUp() {
         User user = User.builder()
             .nickname("yunjae01")
             .email("yunjae01@naver.com")
@@ -28,11 +27,31 @@ class UserRepositoryTest {
             .build();
 
         userRepository.save(user);
+    }
+
+    @DisplayName("이메일로 유저 검색 성공")
+    @Test
+    void find_by_email_success() {
+        // given
+        String email = "yunjae01@naver.com";
 
         // when
-        User findUser = userRepository.findByEmail("yunjae01@naver.com").orElseThrow();
+        User findUser = userRepository.findByEmail(email).orElseThrow();
 
         // then
-        assertThat(findUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(findUser.getEmail()).isEqualTo(email);
+    }
+
+    @DisplayName("이메일로 유저 존재 확인 성공")
+    @Test
+    void exists_by_email_success() {
+        // given
+        String email = "yunjae01@naver.com";
+
+        // when
+        boolean existsByEmail = userRepository.existsByEmail(email);
+
+        // then
+        assertThat(existsByEmail).isTrue();
     }
 }
