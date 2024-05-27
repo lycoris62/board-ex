fef92980771086edf6915d051d748bc52febf9bb
barami62 <wodbs7758@naver.com>
2024-05-27 17:59:02 +0900

    Fix: @ToString 어노테이션 삭제

--- src/main/java/ex/boards/domain/user/dto/SignupReqDto.java
+++ src/main/java/ex/boards/domain/user/dto/SignupReqDto.java
@@ -2,11 +2,9 @@ package ex.boards.domain.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
-@ToString
 public class SignupReqDto {
 
     private String nickname;
