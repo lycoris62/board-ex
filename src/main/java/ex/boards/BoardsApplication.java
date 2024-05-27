83f7168e2c34537dd6c00b0b15b5402f0e6f0927
barami62 <wodbs7758@naver.com>
2024-05-27 14:04:45 +0900

    first commit

---
+++ src/main/java/ex/boards/BoardsApplication.java
@@ -0,0 +1,12 @@
+package ex.boards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardsApplication.class, args);
+    }
+}
